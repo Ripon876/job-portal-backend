b129fc31f2d4d5f8571a47f7c4c70ec2d48b335d
Ripon876 <islam876ripon@gmail.com>
2024-09-13 14:31:19 +0600

    refactor: replace appConfigs with appConstants and add add related constants

--- src/configs/appConfigs.ts
+++
@@ -1,3 +0,0 @@
-export const PORT = process.env.PORT || 5000;
-export const FRONTEND_BASE =
-  process.env.FRONTEND_BASE || "http://localhost:5173";
---
+++ src/constants/appContants.ts
@@ -0,0 +1,13 @@
+export const PORT = process.env.PORT || 5000;
+export const FRONTEND_BASE =
+  process.env.FRONTEND_BASE || "http://localhost:5173";
+export const BYCRYPT_SALT_ROUND = process.env.BYCRYPT_SALT_ROUND || 10;
+export const ADMIN_DOMAIN = process.env.ADMIN_DOMAIN || "test.com";
+
+export const JWT_SECRET = process.env.JWT_SECRET || "my-super-secret";
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "7d";
+
+export const DB_USER_NAME = process.env.DB_USER_NAME || "admin";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "admin";
+export const DB_NAME = process.env.DB_NAME || "job-portal";
+export const DB_HOST_NAME = process.env.DB_HOST_NAME || "mydb";
